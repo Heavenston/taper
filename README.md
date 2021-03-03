36cb69f479ae4a61f8b95bbc52b2ca278949774c
Heavenstone <malolegendrelemaire@gmail.com>
2021-03-03 18:09:45 +0100

    added better examples

--- README.md
+++ README.md
@@ -10,38 +10,9 @@ If disabled, OS Threads are used instead
 
 While this may be overkill for Clients, this may be a great performance improvement for Servers.
 
-## Server Usage
+## Documentation And Usage
 
-```rust
-use taper::Server;
-
-// Try to bind server for listening on localhost and port 1234
-// Using u32 packets
-let server = Server::<u32>::bind("127.0.0.1:1234").unwrap();
-
-// Wait for the connection of a single socket
-let socket = server.event_receiver().recv().unwrap();
-
-// Do whatever you want with the socket!
-```
-
-## Client Usage
-
-```rust
-use taper::{Socket, SocketEvent};
-
-let socket = Socket::<u32>::connect("127.0.0.1:1234").unwrap();
-
-socket.packet_sender().send(56745).unwrap();
-
-loop {
-    match socket.event_receiver().recv().unwrap() {
-        SocketEvent::Packet(packet) => println!("Received a packet from the server: {}", packet),
-        SocketEvent::InvalidPacket => println!("The server sent an invalid packet :("),
-        SocketEvent::IoError(error) => println!("An error occurred {}", error),
-    }
-}
-```
+[Documentation](https://docs.rs/taper)
 
 ## Contributing
 
--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,59 @@
 //!
 //! While this may be overkill for Clients, this may be a great performance improvement for Servers.
 //!
-//! ## Usage
+//! ## Examples
 //!
-//! See [`Server`] and [`Socket`] documentations for example usage
+//!
+//! ### Server example
+//! ```no_run
+//! use taper::{Server, ServerEvent, SocketEvent};
+//!
+//! // Try to bind server for listening on localhost and port 1234 using u32 packets
+//! let server = Server::<u32, u32>::bind("127.0.0.1:1234").expect("Could not bind the server");
+//!
+//! // Wait for the connection of a single socket
+//! let socket = match server.event_receiver().recv().unwrap() {
+//!     ServerEvent::Socket(socket) => socket,
+//!     ServerEvent::IoError(e) => panic!(e),
+//! };
+//!
+//! let packet = match socket.event_receiver().recv().unwrap() {
+//!     SocketEvent::Packet(packet) => packet,
+//!     SocketEvent::InvalidPacket => panic!("Received an invalid packet"),
+//!     SocketEvent::IoError(e) => panic!(e),
+//! };
+//!
+//! // Print the first packet received
+//! // With the client example below this would print 10
+//! println!("Received {} from the remote socket !", packet);
+//!
+//! // Reply to the socket
+//! // 11 with the client example
+//! socket.packet_sender().send(packet + 1).unwrap();
+//! ```
+//!
+//! ### Client Example
+//!
+//! ```no_run
+//! use taper::{Socket, SocketEvent};
+//!
+//! // Connect to localhost server using u32 packets
+//! let socket = Socket::<u32, u32>::connect("127.0.0.1:1234").expect("Could not connect to local server");
+//!
+//! // Send the value '10'
+//! socket.packet_sender().send(10).unwrap();
+//!
+//! // Wait for a response packet
+//! let response_packet = match socket.event_receiver().recv().unwrap() {
+//!     SocketEvent::Packet(packet) => packet,
+//!     SocketEvent::InvalidPacket => panic!("Received an invalid packet"),
+//!     SocketEvent::IoError(e) => panic!(e),
+//! };
+//!
+//! // With the server example above this would be '11'
+//! println!("Received the value '{}' from the server", response_packet);
+//!
+//! ```
 
 mod server;
 mod socket;
